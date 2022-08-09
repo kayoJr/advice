46ec380458e9da9a67b876564e952496fe35330c
kayoJr <kalebmelaku97@gmail.com>
2022-08-09 20:56:57 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#293242',
+        medBlue: '#374256',
+        chemicalGreen: '#53ffab',
+        hoverGreen: '#00FF80',
+        lightBlue: '#cfe3ea',
+      }
+    },
+  },
+  plugins: [],
+}
